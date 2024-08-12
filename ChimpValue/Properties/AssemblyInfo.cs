fc8102a30cd984ef8628bfacc2de1437f3014691
Wilps <demi@wilps.ru>
2024-08-13 04:32:27 +0700

    Add files via upload

---
+++ ChimpValue/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using ChimpValue;
+
+[assembly: MelonInfo(typeof(ChimpValueMod), "ChimpValue", "1.0.1", "Wilps")]
+[assembly: MelonGame("Firefly Studios", "Stronghold 1 Definitive Edition")]
+[assembly: AssemblyTitle("ChimpValue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChimpValue")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0A432197-1294-42FF-B25E-8FAABC5752A0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
